986fe0f7135618be3bfdfd33945ebdfcf4d09924
Christopher Bowman <chrisbowmanfl@gmail.com>
2023-10-09 16:34:39 +0900

    Add JavaScript Create Grid

---
+++ script.js
@@ -0,0 +1,26 @@
+// Make function to create grid into 16 by 16 divs
+
+console.log("I'm alive!")
+
+function makeGrid() {
+  for (let i = 0; i <= 256; i++) {
+    let div = document.createElement('div');
+    div.classList.add('cell');
+    div.addEventListener('click', function(event) {
+      event.target.style.backgroundColor = 'black';
+      console.log(event);
+    })
+    grid.appendChild(div);
+  }
+}
+
+// Select the container
+
+const grid = document.querySelector("#grid");
+
+// Add into the container
+
+// Trigger different class by clicking grid cell
+
+
+makeGrid();
