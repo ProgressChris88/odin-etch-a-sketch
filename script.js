c9142b7f910ec8fb0b2b51b5c1322eded1cd2a28
Christopher Bowman <chrisbowmanfl@gmail.com>
2023-10-24 20:30:39 +0900

    Add Grid Size

--- script.js
+++ script.js
@@ -2,17 +2,30 @@
 
 console.log("I'm alive!");
 
-function makeGrid() {
-  for (let i = 1; i <= 256; i++) {
+// Allow for a grid choice. Default to 16 by 16.
+
+function makeGrid(gridChoice = 32) {
+  grid.innerHTML = "";
+  for (let i = 1; i <= gridChoice * gridChoice; i++) {
     let div = document.createElement("div");
+    div.style.cssText = `width: ${500 / gridChoice - 2}px; height: ${
+      500 / gridChoice - 2
+    }px;`;
     div.classList.add("cell");
-    div.addEventListener("click", function (event) {
+    div.addEventListener("mouseover", function (event) {
       event.target.style.backgroundColor = "black";
       console.log(event);
     });
     grid.appendChild(div);
   }
 }
+const button = document.querySelector(".button");
+
+button.addEventListener("click", () => {
+  console.log("clicked");
+  let userChoice = prompt("How big do you want the grid?");
+  makeGrid(userChoice);
+});
 
 // Select the container
 
@@ -21,3 +34,7 @@ const grid = document.querySelector("#grid");
 // Trigger different class by clicking grid cell
 
 makeGrid();
+
+function clearBox(elementID) {
+  document.getElementById(elementId).innerHTML = "";
+}
